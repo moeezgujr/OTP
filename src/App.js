6d7e650b43e4a556126fb593f4c2acbbaa72950c
moeeztarik <mtariq@knowledgeplatform.com>
2023-05-14 18:21:47 +0500

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,3 @@
-import React, { useEffect } from 'react';
-
 const Home = () => {
 
 
